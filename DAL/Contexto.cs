3598847262e9ec557343944dc6e031ec30f63b36
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-03 14:16:26 -0400

    Creacion del Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosTecnico.Models;
+
+namespace RegistrosTecnico.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options){}
+
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+}
