0ffee8a77f2583ca4586a2f92e767f35924461ef
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-03 14:04:35 -0400

    Creacion del modelo Tecnico

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnico.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
+    [Required(ErrorMessage = "El Nombres obligatorio")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Favor de introducir el sueldo por hora del técnico.")]
+    [Range(0, 200000, ErrorMessage = "Favor de introducir un sueldo mayor que 1 y menor que 200000.")]
+    public float SueldoHora { get; set; }
+
+}
