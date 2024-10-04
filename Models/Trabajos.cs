1926e6a52503865534a53bb155cfbd2ae31a2c26
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-04 00:15:13 -0400

    Creacion del modelo trabajo y inyeccion en el contexto

---
+++ Models/Trabajos.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnico.Models;
+
+public class Trabajos
+{
+    [Key]
+    public int TrabajoId { get; set; }
+
+    [StringLength(250)]
+    [Required(ErrorMessage = "Campo obligatario")]
+    public string? Descripcion { get; set; }
+
+    public DateTime? Fecha { get; set; } = DateTime.Now;
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public double Monto { get; set; }
+
+    [ForeignKey("Tecnicos")]
+    [Required(ErrorMessage = "Debe de elegir un tecnico")]
+    public int TecnicoId { get; set; }
+    public Tecnicos? Tecnicos { get; set; }
+
+    [ForeignKey("Clientes")]
+    [Required(ErrorMessage = "Debe de elegir un cliente")]
+    public int? ClienteId { get; set; }
+    public Clientes? Cliente { get; set; }
+}
