d0bf08746be435583022622d4978e247870dd950
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-03 23:15:57 -0400

    Creacion del modelo cliente

---
+++ Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnico.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClientesId { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Favor de introducir el nombre del Cliente.")]
+    public string? Nombres { get; set; }
+    [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten números.")]
+    public string WhatsApp { get; set; }
+
+
+
+}
