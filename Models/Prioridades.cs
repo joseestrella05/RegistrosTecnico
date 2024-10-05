ceb4bc9ce89894df06d927ce7f5ee802d513c72d
jose gabriel <jose gabriel@JOSEGABRIEL>
2024-10-05 12:46:24 -0400

    Creacion del modelo prioridad

---
+++ Models/Prioridades.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTecnico.Models;
+
+public class Prioridades
+{
+	[Key]
+	public int PrioridadId { get; set; }
+	[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
+	[Required(ErrorMessage = "El Campo Descripción es obligatorio")]
+	public string? Descripcion { get; set; }
+
+	public int Tiempo { get; set; }
+}
